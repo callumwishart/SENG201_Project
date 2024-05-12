52abf314c832a5523dbc8adc2dc57d990b67bf4c
Callum <cwi153@uclive.ac.nz>
2024-05-13 11:38:01 +1200

    Created Consumable, Purchasabe, Item, and Resource and updated Round, Tower, Upgrade

---
+++ src/main/java/seng201/team0/models/Purchasable.java
@@ -0,0 +1,5 @@
+package seng201.team0.models;
+
+public interface Purchasable {
+    public double getCost();
+}
--- src/main/java/seng201/team0/models/Upgrade.java
+++ src/main/java/seng201/team0/models/Upgrade.java
@@ -1,4 +1,12 @@
 package seng201.team0.models;
 
-public class Upgrade {
+public class Upgrade extends Item{
+    int upgradeCount;
+    public Upgrade(String inputName, String inputDescription) {
+        super(inputName, inputDescription);
+    }
+    public void applyUpgrade() {
+        // Yet to implement
+    }
+
 }
