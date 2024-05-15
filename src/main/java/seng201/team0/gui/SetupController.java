b443f8d3bc82ab0de0fe288a84653cf970d633ad
Callum <cwi153@uclive.ac.nz>
2024-05-16 11:16:51 +1200

    Created SetupController and initialised the components

---
+++ src/main/java/seng201/team0/gui/SetupController.java
@@ -0,0 +1,22 @@
+package seng201.team0.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+
+import java.awt.*;
+
+public class SetupController {
+    @FXML
+    private TextField nameField;
+    @FXML
+    private RadioButton normalRadioBtn, hardRadioBtn;
+    @FXML
+    private Slider roundSlider;
+    @FXML
+    private Button t1Btn, t2Btn, t3Btn, t4Btn, t5Btn, selectedT1Btn, selectedT2Btn, selectedT3Btn;
+    @FXML
+    private Label roundIndicatorLabel, selectedTowerLabel, towerResourceLabel, resourceAmountLabel, resourceValueLabel1, towerSpeedLabel;
+    
+
+}
