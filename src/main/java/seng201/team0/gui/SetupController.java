7541aeb015d063fe5c5d71ae53966f28889d7e28
Callum <cwi153@uclive.ac.nz>
2024-05-16 11:20:00 +1200

    Created StartController and fixed imports for setup controller

--- src/main/java/seng201/team0/gui/SetupController.java
+++ src/main/java/seng201/team0/gui/SetupController.java
@@ -3,8 +3,10 @@ package seng201.team0.gui;
 import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Slider;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 
-import java.awt.*;
 
 public class SetupController {
     @FXML
@@ -17,6 +19,6 @@ public class SetupController {
     private Button t1Btn, t2Btn, t3Btn, t4Btn, t5Btn, selectedT1Btn, selectedT2Btn, selectedT3Btn;
     @FXML
     private Label roundIndicatorLabel, selectedTowerLabel, towerResourceLabel, resourceAmountLabel, resourceValueLabel1, towerSpeedLabel;
-    
+
 
 }
---
+++ src/main/java/seng201/team0/gui/StartController.java
@@ -0,0 +1,9 @@
+package seng201.team0.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class StartController {
+    @FXML
+    private Button startBtn;
+}
