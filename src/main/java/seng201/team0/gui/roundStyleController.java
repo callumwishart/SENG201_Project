a0b588055ff12cf2c374ed7c99f5ad34fc96b505
Callum <cwi153@uclive.ac.nz>
2024-05-16 12:54:18 +1200

    Created game over controller, inventory controller, rand event controller, round style controller, round summary controller

---
+++ src/main/java/seng201/team0/gui/roundStyleController.java
@@ -0,0 +1,9 @@
+package seng201.team0.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+
+public class roundStyleController {
+    @FXML
+    private RadioButton safeRadioBtn, steadyRadioBtn, riskyRadioBtn;
+}
---
+++ src/main/java/seng201/team0/gui/roundSummaryController.java
@@ -0,0 +1,12 @@
+package seng201.team0.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class roundSummaryController {
+    @FXML
+    private Label playerLabel, winLabel, coinsLabel, pointsLabel, roundLabel;
+    @FXML
+    private ImageView t1Img, t2Img, t3Img, t4Img, t5Img;
+}
