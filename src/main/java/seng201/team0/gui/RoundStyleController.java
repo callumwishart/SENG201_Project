d8c4bc2c2dd542a347af8239ea3c5a60697d168d
Theo-Parker <theoparker111@gmail.com>
2024-05-18 11:54:29 +1200

    Refactor roundstyle and roundsummary names to Capitals

--- src/main/java/seng201/team0/gui/RoundStyleController.java
+++ src/main/java/seng201/team0/gui/RoundStyleController.java
@@ -3,7 +3,7 @@ package seng201.team0.gui;
 import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
 
-public class roundStyleController {
+public class RoundStyleController {
     @FXML
     private RadioButton safeRadioBtn, steadyRadioBtn, riskyRadioBtn;
 }
