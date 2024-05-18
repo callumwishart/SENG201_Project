4e177783845d861a4bff09faf887ecc36e05457a
Theo-Parker <theoparker111@gmail.com>
2024-05-18 16:04:49 +1200

    update GameObserver doc

--- src/main/java/seng201/team0/models/gameplay/GameObserver.java
+++ src/main/java/seng201/team0/models/gameplay/GameObserver.java
@@ -2,10 +2,10 @@ package seng201.team0.models.gameplay;
 
 public interface GameObserver {
     /**
-     * @author Theo Parker
-     * the observe function should be implemented such that the observer class collects the state
-     * of the carts and towers active during the round run by gameRunner and represent it graphically
-     * or use it for testing purposes.
+     * The observe function should be implemented such that the observer class
+     * collects the state of the carts and towers active during the round
+     * run by gameRunner and represent it graphically or use it for
+     * testing purposes.
      */
     public void observe(GameRunner gameRunner);
 }
