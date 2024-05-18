94e498652d688721dc227cde11e39bba587202d0
Theo-Parker <theoparker111@gmail.com>
2024-05-18 16:27:31 +1200

    implement GameRunner class with gameplay logic, awaiting dependent classes to be finished (WIP)

--- src/main/java/seng201/team0/models/gameplay/GameRunner.java
+++ src/main/java/seng201/team0/models/gameplay/GameRunner.java
@@ -1,15 +1,84 @@
 package seng201.team0.models.gameplay;
 
+import seng201.team0.exceptions.FullCartException;
+import seng201.team0.models.resources.Resource;
+import seng201.team0.models.towers.Tower;
+
 import java.util.ArrayList;
 
 public class GameRunner{
 
     GameObserver observer;
     ArrayList<Cart> carts;
+    Round round;
 
     GameRunner(Round round, GameObserver observer){
         this.observer = observer;
         this.carts = round.getCarts();
+        this.round = round;
+    }
+
+    public boolean run() throws InterruptedException {
+        boolean finished = false;
+        int roundTimeElapsed = 0;
+
+        while (!finished){
+            Thread.sleep(1000); // sleep 1 second
+            roundTimeElapsed += 1;
+            for (Cart cart : carts){
+                if(cart.getDistance() < round.trackDistance){
+                    cart.incrementDistance();
+                    if (cart.getDistance() >= round.trackDistance){
+                        cart.setFinished(true);
+                    }
+                }
+            }
+            for (Tower tower : round.towers){
+                if (tower.getReloadTimeElapsed() % tower.getReloadSpeed() == 0){
+                    tower.setReloading(false);
+                }
+                else{
+                    tower.incrementReloadTimeElapsed();
+                }
+                for (Cart cart : carts){
+                    if (tower.isReloading() || cart.isFull() || cart.isFinished()){
+                        break;
+                    }
+                    else{
+                        for (int i = 0; i < tower.getResourceAmount(); i++){
+                            try {
+                                cart.fillCart(new Resource(tower.getResourceType()));
+                            } catch (FullCartException e) {
+                                tower.setReloading(true);
+                                tower.incrementReloadTimeElapsed();
+                                break; // Cart has been filled
+                            }
+                        }
+                    }
+                }
+            }
+            // check if all carts have finished
+            finished = true;
+            for (Cart cart : carts){
+                if (!cart.isFinished()){
+                    finished = false; // at least one cart isn't finished
+                    break; // continue with gameplay
+                }
+            }
+            this.observer.observe(this); // passes the observer the GameRunner instance for inspection
+        }
+
+        boolean gameSuccess = true;
+        // Determine win or loss
+        for (Cart cart : carts){
+            if (!cart.isFull()){
+                gameSuccess = false;
+                break;
+            }
+        }
+
+        return gameSuccess;
+
     }
 
 
