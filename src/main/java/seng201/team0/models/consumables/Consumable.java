a4823b7a6be3b3089ee992a56ac140c6b30d594c
Theo-Parker <theoparker111@gmail.com>
2024-05-19 22:26:58 +1200

    Update consumables to work with GameRunner properly

--- src/main/java/seng201/team0/models/consumables/Consumable.java
+++ src/main/java/seng201/team0/models/consumables/Consumable.java
@@ -1,16 +1,13 @@
 package seng201.team0.models.consumables;
 
 import seng201.team0.models.Item;
+import seng201.team0.models.gameplay.Round;
 
-public class Consumable extends Item {
+public abstract class Consumable extends Item {
     public Consumable(String inputName, String inputDescription, int inputCost) {
         super(inputName, inputDescription, inputCost);
     }
-    public void apply() {
-
-    };
-    public void remove() {
-
-    };
+    public abstract void apply(Round round);
+    public abstract boolean remove(Round round);
 
 }
