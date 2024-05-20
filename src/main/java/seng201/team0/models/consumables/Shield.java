721da27b9ff2f21a942c7aeceb29214d880eed9e
Theo-Parker <theoparker111@gmail.com>
2024-05-20 16:58:09 +1200

    Upgrade edits to work with shop

--- src/main/java/seng201/team0/models/consumables/Shield.java
+++ src/main/java/seng201/team0/models/consumables/Shield.java
@@ -3,8 +3,8 @@ package seng201.team0.models.consumables;
 import seng201.team0.models.gameplay.Round;
 
 public class Shield extends Consumable {
-    public Shield(String inputName, String inputDescription, int inputCost) {
-        super(inputName, inputDescription, inputCost);
+    public Shield() {
+        super("Shield", "Protects you from one cart finishing without being filled", 500);
     }
 
     public void apply(Round round) {
