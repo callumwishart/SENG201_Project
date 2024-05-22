82c55319da1a9c5782b92a14e9f888d5e3768d5d
Callum <cwi153@uclive.ac.nz>
2024-05-23 01:08:47 +1200

    Finished JavaDoc for Tower and all types of towers

--- src/main/java/seng201/team0/models/towers/Tower.java
+++ src/main/java/seng201/team0/models/towers/Tower.java
@@ -10,6 +10,12 @@ import java.util.ArrayList;
 
 import static java.lang.Math.round;
 
+/**
+ * Tower Class
+ * <p>
+ *     Houses all of the methods of tower
+ * </p>
+ */
 public class Tower extends Item implements Sellable {
     double inputCost;
     int resourceAmount;
@@ -25,6 +31,9 @@ public class Tower extends Item implements Sellable {
     int used;
     ArrayList<Upgrade> upgrades = new ArrayList<>();
 
+    /**
+     * Initialises the tower and sets the resourceAmount to 1;
+     */
     public Tower (Resource inputResourceType, int inputReloadSpeed, String inputName, String inputDescription, int inputCost, String initialImagePath, String brokenImage) {
         super(inputName, inputDescription, inputCost);
         resourceAmount = 1;
@@ -36,33 +45,61 @@ public class Tower extends Item implements Sellable {
         brokenImagePath = brokenImage;
     }
 
+    /**
+     * @hidden
+     */
     public int getResourceAmount() {
         return resourceAmount;
     }
+
+    /**
+     * @hidden
+     */
     public void increaseResourceAmount(int amount) {
         resourceAmount += amount;
     }
 
+    /**
+     * @hidden
+     */
     public Resource getResource() {
         return resourceType;
     }
 
+    /**
+     * Increases the tower level by 1
+     */
     public void levelUp (){
         level += 1;
     }
 
+    /**
+     * @hidden
+     */
     public ArrayList<Upgrade> getUpgrades() {
         return upgrades;
     }
 
+    /**
+     * Increases the reload speed by the specified amount
+     */
     public void increaseReloadSpeed(int amount) {
         reloadSpeed += amount;
     }
 
+    /**
+     * @hidden
+     */
     public boolean isReloading() {
         return isReloading;
     }
 
+    /** Sets the tower to be in a reloading state
+     * <p>
+     *     If the input is false it will reset the reload time
+     * </p>
+     * @param bool value of what the tower isReloading should be set to
+     */
     public void setReloading(boolean bool){
         this.isReloading = bool;
         if (!bool){
@@ -70,47 +107,91 @@ public class Tower extends Item implements Sellable {
         }
     }
 
+    /**
+     * @hidden
+     */
     public int getReloadTimeElapsed() {
         return reloadTimeElapsed;
     }
 
+    /**
+     * @hidden
+     */
     public void incrementReloadTimeElapsed(){
         reloadTimeElapsed += 1;
     }
 
+    /**
+     * The upgrade point limit is determined by the level of the tower multiplied by 200
+     */
     public int getUpgradePointLimit() {
         return level * 200;
     }
 
+    /**
+     * Sets the tower to broken and updates imagePath
+     */
     public void setToBroken () {
         isBroken = true;
         imagePath = brokenImagePath;
     }
+
+    /**
+     * Sets the tower to fixed and updates the image path
+     */
     public void setToFixed() {
         isBroken = false;
         imagePath = fixedImagePath;
     }
+
+    /**
+     * @hidden
+     */
     public int getUsed() {
         return used;
     }
+
+    /**
+     * @hidden
+     */
     public int getRepairCost() {
         return (int) (this.getCost() / 2);
     }
+
+    /**
+     * @hidden
+     */
     public Boolean isBroken() {
         return isBroken;
     }
 
+    /**
+     * @hidden
+     */
     public int getReloadSpeed() {
         return reloadSpeed;
     }
 
+    /**
+     * @hidden
+     */
     public String getImagePath() {
         return imagePath;
     }
+
+    /**
+     * @hidden
+     */
     public int getLevel() {
         return level;
     }
 
+    /**
+     * Gets the Sell cost of the tower
+     * <p>
+     *     If the tower is broken then the sell cost is 0 otherwise it is half of the cost of it
+     * </p>
+     */
     public int getSellCost() {
         if (!this.isBroken()) {
             return (int) (this.getCost() / 2);
@@ -119,11 +200,20 @@ public class Tower extends Item implements Sellable {
         }
     }
 
+    /**
+     * Add upgrade to tower
+     * <p>
+     *     Adds the upgrade to the tower and also levels up the tower.
+     * </p>
+     */
     public void addUpgrade(Upgrade upgrade){
         this.upgrades.add(upgrade);
         this.levelUp();
     }
 
+    /**
+     * @hidden
+     */
     public void incrementUsed() {
         this.used += 1;
     }
