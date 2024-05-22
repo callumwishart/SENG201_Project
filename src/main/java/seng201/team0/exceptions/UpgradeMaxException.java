2e9ecbf157818b3fdb73a8e57e6e4218b5b6e43b
Theo-Parker <theoparker111@gmail.com>
2024-05-23 01:30:06 +1200

    create upgrademaxexception

---
+++ src/main/java/seng201/team0/exceptions/UpgradeMaxException.java
@@ -0,0 +1,11 @@
+package seng201.team0.exceptions;
+
+public class UpgradeMaxException extends Exception {
+    public UpgradeMaxException() {
+        super();
+    }
+
+    public UpgradeMaxException(String message) {
+        super(message);
+    }
+}
