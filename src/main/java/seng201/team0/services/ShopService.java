932739095d8c9e2b4961a069b79e2b625f9401a1
Callum <cwi153@uclive.ac.nz>
2024-05-23 04:43:07 +1200

    Created Javadoc for PlayerService, ShopService, and TransationService

--- src/main/java/seng201/team0/services/ShopService.java
+++ src/main/java/seng201/team0/services/ShopService.java
@@ -9,11 +9,30 @@ import seng201.team0.models.consumables.Consumable;
 import seng201.team0.models.towers.Tower;
 import seng201.team0.models.upgrades.Upgrade;
 
+/**
+ * Shop Service class
+ * <p>
+ *     This class is used so that the gui does not directly interact with the shop
+ * </p>
+ */
 public class ShopService {
-
+    /**
+     * Inventory service holds the inventory service so that it can make changes in the inventory
+     */
     InventoryService inventoryService;
+    /**
+     * shop holds the instance of shop that this ShopService should manage
+     */
     Shop shop;
 
+    /**
+     * Shop Service Constructor
+     * <p>
+     *     Assigns {@code shop} to {@code this.shop} and {@code inventoryService} to {@code this.inventoryService}
+     * </p>
+     * @param shop is the shop that this service will manage
+     * @param inventoryService is the inventoryService that this shop service can interact with
+     */
     public ShopService (Shop shop, InventoryService inventoryService) {
         this.shop = shop;
         this.inventoryService = inventoryService;
@@ -39,12 +58,35 @@ public class ShopService {
         }
     }
 
+    /**
+     * Purchase Upgrade Method
+     * <p>
+     *     This method first checks if there is space for an upgrade using {@code inventoryService.checkUpgradeSpace()}
+     *     then purchases the upgrade using {@code TransactionService.purchase()}, then adds the upgrade to the
+     *     inventory using {@code inventoryService.addUpgrade()}
+     * </p>
+     * @param upgrade is the upgrade that is being purchased
+     * @throws PurchaseException is thrown when the item cannot be purchased
+     * @throws UpgradesFullException is thrown when the inventory is full
+     */
     public void purchaseUpgrade(Upgrade upgrade) throws PurchaseException, UpgradesFullException {
         inventoryService.checkUpgradeSpace();
         TransactionService.purchase(upgrade.getCost(), inventoryService);
         inventoryService.addUpgrade(upgrade);
     }
 
+    /**
+     * Purchase Consumable Method
+     * <p>
+     *     This method first checks if this consumable is already present and throws an
+     *     {@code ActiveConsumerException} if so, otherwise it purchases the consumable using
+     *     {@code TransactionService.purchase()}. It then adds the consumable to the inventory using
+     *     {@code this.inventoryService.addConsumable()}
+     * </p>
+     * @param consumable is the consumable that is being purchased
+     * @throws ActiveConsumableException is thrown if the consumable is already active
+     * @throws PurchaseException is thrown if the item cannot be purchased
+     */
     public void purchaseConsumable(Consumable consumable) throws ActiveConsumableException, PurchaseException {
         for (Consumable activeConsumable : this.inventoryService.getConsumables()){
             if (activeConsumable.getClass() == consumable.getClass()) {
