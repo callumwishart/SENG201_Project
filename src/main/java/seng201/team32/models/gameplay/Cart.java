f75100e2f6fbebc7d1d89a43187a3e6dc96da6d6
Theo <theoparker111@gmail.com>
2024-05-24 02:36:33 +1200

    gameplay docs almost done

--- src/main/java/seng201/team32/models/gameplay/Cart.java
+++ src/main/java/seng201/team32/models/gameplay/Cart.java
@@ -3,6 +3,9 @@ package seng201.team32.models.gameplay;
 import seng201.team32.exceptions.FullCartException;
 import seng201.team32.models.resources.Resource;
 
+/**
+ * The Cart class encapsulates all cart-related data for use in round creation and gameplay.
+ */
 public class Cart {
     private String name;
     private int size;
@@ -14,6 +17,12 @@ public class Cart {
     private int distance;
     private boolean finished;
 
+    /**
+     * The Cart constructor
+     * @param size is an int that represents how many resources a cart can accomodate
+     * @param speed is the distance per second that the cart travels at
+     * @param resource is the resource that will be used by the towers to determine whether it can be filled by a particular tower
+     */
     public Cart(int size, int speed, Resource resource) {
         this.size = size;
         this.cargo = new Resource[this.size];
@@ -25,6 +34,11 @@ public class Cart {
         this.name = String.format("%s Cart", this.resource.getResourceType());
     }
 
+    /**
+     * The Universal Cart constructor, providing no resource to the {@code Cart()} constructor will make it universal, so all towers can fill it.
+     * @param size is an int that represents how many resources a cart can accomodate
+     * @param speed is the distance per second that the cart travels at
+     */
     Cart(int size, int speed) {
         this.name = "Universal Cart";
         this.size = size;
@@ -35,34 +49,67 @@ public class Cart {
         this.distance = 0;
     }
 
+    /**
+     * Gets the size of the cart
+     * @return size of cart
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     * Gets the resource of the cart
+     * @return the resource of cart
+     */
     public Resource getResource() {
         return resource;
     }
 
+    /**
+     * Returns a boolean where true means that the cart is universal
+     * @return boolean
+     */
     public boolean isUniversal() {
         return universal;
     }
 
+    /**
+     * Gets the speed of the cart
+     * @return cart speed
+     */
     public int getSpeed() {
         return speed;
     }
 
+    /**
+     * Gets the distance that the cart has traveled
+     * @return int distance travelled
+     */
     public int getDistance(){
         return this.distance;
     }
 
+    /**
+     * Changes the speed of the cart
+     * @param amount how much to change the speed
+     */
     public void changeSpeed(int amount) {
         speed += amount;
     }
 
+    /**
+     * Returns a boolean that indicates whether the cart is full or not
+     * @return boolean indicating if cart is full
+     */
     public boolean isFull() {
         return this.cargoSlotsFilled == this.size;
     }
 
+    /**
+     * Attempts to fill the cart with the resource provided, adding it to the cargo list
+     * @param resource the resource to be added to cargo
+     * @throws FullCartException cannot add, the cart is full
+     */
     public void fillCart(Resource resource) throws FullCartException {
         if (this.isFull()){
             throw new FullCartException(String.format("Cart %s is full", this.name));
@@ -79,22 +126,42 @@ public class Cart {
         distance += speed;
     }
 
+    /**
+     * Returns whether the cart is finished or not
+     * @return cart's finished property
+     */
     public boolean isFinished() {
         return finished;
     }
 
+    /**
+     * Sets the finished state of the cart to indicate if it's finished going the distance
+     * @param state indicates if the cart is finished
+     */
     public void setFinished(boolean state) {
         finished = state;
     }
 
+    /**
+     * Returns the cart's name
+     * @return cart name
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Gets the cargo slots that have been filled according to the cargoSlotsFilled property
+     * @return cart cargoSlotsFilled
+     */
     public int getCargoSlotsFilled() {
         return cargoSlotsFilled;
     }
 
+    /**
+     * Gets the cargo list of the cart
+     * @return cart cargo list
+     */
     public Resource[] getCargo() {
         return this.cargo;
     }
--- src/main/java/seng201/team32/models/gameplay/GameObserver.java
+++ src/main/java/seng201/team32/models/gameplay/GameObserver.java
@@ -8,14 +8,27 @@ import seng201.team32.exceptions.TowerNotFoundException;
  */
 public interface GameObserver {
     /**
-     * The observe function should be implemented such that the observer class
-     * collects the state of the carts and towers active during the round
-     * run by gameRunner and represent it graphically or use it for
-     * testing purposes.
+     *  The observe function should be implemented such that the observer class
+     *  collects the state of the carts and towers active during the round
+     *  run by gameRunner and represent it graphically or use it for
+     *  testing purposes.
+     * @param gameRunner the game runner class to observe data from
      */
     void observe(GameRunner gameRunner);
 
+    /**
+     * The win function should be implemented such that coins/points are calculated to be added to the
+     * player's inventory, and the necessary steps are taken to facilitate the continuation of gameplay
+     * @param coins the coins won from the round
+     * @param points the points won from the round
+     * @throws NegativeAdditionException in the case that the points/coins values become negative and the function body tries to add to the inventory
+     * @throws TowerNotFoundException thrown if the tower isn't found when running random event related functions after a game is won
+     */
     void win(int coins, int points) throws NegativeAdditionException, TowerNotFoundException;
 
+    /**
+     * The lose function should be implemented such that the game loss events are triggered and the game ends with a summary.
+     * @throws TowerNotFoundException thrown if the tower isn't found when running random event related functions after a game is won
+     */
     void lose() throws TowerNotFoundException;
 }
