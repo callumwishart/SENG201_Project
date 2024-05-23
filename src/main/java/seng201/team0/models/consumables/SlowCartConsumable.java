141f6ed65be0f4c0a6a7c14af515c00022bed557
Theo <theoparker111@gmail.com>
2024-05-23 22:36:57 +1200

    rm random cost var

--- src/main/java/seng201/team0/models/consumables/SlowCartConsumable.java
+++ src/main/java/seng201/team0/models/consumables/SlowCartConsumable.java
@@ -5,7 +5,6 @@ import seng201.team0.models.gameplay.Cart;
 import seng201.team0.models.gameplay.Round;
 
 public class SlowCartConsumable extends Consumable {
-    int cost = 20;
 
     public SlowCartConsumable() {
         super("Slow Carts", "Slows all carts by 1 for 1 round", 350);
