1899333780c8e22e844fa8fbdafc5b322cfcdf60
Callum <cwi153@uclive.ac.nz>
2024-05-23 22:59:24 +1200

    Ceated javadocs for consumables

--- src/main/java/seng201/team0/models/consumables/SlowCartConsumable.java
+++ src/main/java/seng201/team0/models/consumables/SlowCartConsumable.java
@@ -4,12 +4,26 @@ package seng201.team0.models.consumables;
 import seng201.team0.models.gameplay.Cart;
 import seng201.team0.models.gameplay.Round;
 
+/**
+ * Slow cart consumable which will make all the carts slower for that round thus making it easier for them to be filled
+ * before they reach the end of the track.
+ */
 public class SlowCartConsumable extends Consumable {
-
+    /**
+     * Constructor which sets the name and description and cost of the consumable
+     */
     public SlowCartConsumable() {
         super("Slow Carts", "Slows all carts by 1 for 1 round", 350);
     }
 
+    /**
+     * This will apply the effect of the consumable to the carts
+     * <p>
+     *     This will iterate through all the carts in the round and remove one (1) from the cart speed if
+     *     the cart speed is greater than one (1)
+     * </p>
+     * @param round round that this is applied to
+     */
     public void apply(Round round) {
         for (Cart cart : round.getCarts()) {
             if (cart.getSpeed() > 1){
@@ -18,6 +32,11 @@ public class SlowCartConsumable extends Consumable {
         }
     }
 
+    /**
+     * Remove function does not do anything as carts do not need to be reset
+     * @param round round that this is applied to
+     * @return boolean if the method has happened
+     */
     public boolean remove(Round round) {
         return true;
     }
--- src/main/java/seng201/team0/models/consumables/TowerSpeedBooster.java
+++ src/main/java/seng201/team0/models/consumables/TowerSpeedBooster.java
@@ -6,14 +6,31 @@ import seng201.team0.models.towers.Tower;
 
 import java.util.ArrayList;
 
+/**
+ * Tower Speed booster which will boost the speed of all towers for the round
+ */
 public class TowerSpeedBooster extends Consumable {
-    // This booster makes the tower production faster by some factor (maybe tied to difficulty?)
+    /**
+     * list of towers that will be affected
+     */
     private ArrayList<Tower> towersAffected = new ArrayList<>();
 
+    /**
+     * Constructor which sets the name and description and cost of the booster
+     */
     public TowerSpeedBooster() {
         super("Tower Speed Boost", "This booster makes the tower production faster.", 350);
     }
 
+    /**
+     * Apply the booster to the towers
+     * <p>
+     *     This method will go through all the towers and for all towers that have a
+     *     reload speed greater than one it will decrease reload speed which results in towers
+     *     reloading faster
+     * </p>
+     * @param round the round that this is applied to
+     */
     public void apply(Round round) {
         for (Tower tower : round.getTowers()) {
             if (tower.getReloadSpeed() > 1){
@@ -22,6 +39,15 @@ public class TowerSpeedBooster extends Consumable {
             }
         }
     }
+
+    /**
+     * Will remove the affect of the booster at the end of the round
+     * <p>
+     *     Gets all the towers in the towers affected and will increase their reload speed
+     * </p>
+     * @param round the round that this will be applied to
+     * @return boolean to show that this has occurred
+     */
     public boolean remove(Round round) {
         for (Tower tower : towersAffected) {
             tower.increaseReloadSpeed();
